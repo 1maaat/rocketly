0cb01b0a3ef672285e8fc9686f50ef19ec3e6707
Pierre Bride <pierre_bride@icloud.com>
2021-08-24 12:39:16 +0200

    maj new and create to artworkscontroller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,6 @@
 class ArtworksController < ApplicationController
+  before_action :set_challenge, only: [:new]
+
   def index
   end
 
@@ -6,19 +8,27 @@ class ArtworksController < ApplicationController
   end
 
   def new
-    @challenge = Challenge.find(params[:challenge_id])
     @artwork = Artwork.new
   end
 
   def create
     @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
     @challenge = Challenge.find(params[:challenge_id])
     @artwork.challenge = @challenge
-    @artwork.save
-    redirect_to challenge_path(@challenge)
+    if @artwork.save
+      flash[:notice] = "Artwork created and published to the #{@challenge.name} !"
+      redirect_to challenge_path(@challenge)
+    else
+      redirect_to challenge_path(@challenge)
+    end
   end
 
   private
+
+  def set_challenge
+    @challenge = Challenge.find(params[:challenge_id])
+  end
 
   def artwork_params
     params.require(:artwork).permit(:title, :description, :selected, :rank)
