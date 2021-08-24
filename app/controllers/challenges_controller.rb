5d087b794334abecbf79e22a1ce4e92dd257a19e
Walid Benzina <walid.benzina@yahoo.fr>
2021-08-24 16:55:04 +0200

    Challenge Show with Artwork

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -11,7 +11,7 @@ class ChallengesController < ApplicationController
     @artworks = @challenge.artworks
   end
 
- def create
+  def create
     @challenge = Challenge.new(challenge_params)
     @challenge.user = current_user
     authorize @challenge
@@ -33,8 +33,6 @@ class ChallengesController < ApplicationController
     params.require(:challenge)
           .permit(:name, :description, :reward, :status)
   end
-
-  private
 
   def set_challenge
     @challenge = Challenge.find(params[:id])
