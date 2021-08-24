a56abbcd8ffb23c2258a893b6150ea3f176292d8
Mathias Salanon <mat.salanon@gmail.com>
2021-08-24 12:57:22 +0200

    added challenge creation

---
+++ app/policies/challenge_policy.rb
@@ -0,0 +1,11 @@
+class ChallengePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'artworks/index'
-  get 'artworks/show'
-  get 'artworks/create'
-  get 'artworks/new'
-  get 'challenges/index'
-  get 'challenges/show'
-  get 'challenges/create'
-  get 'challenges/new'
   devise_for :users
   root to: 'pages#home'
+
+  resources :challenges do
+    resources :artworks, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
