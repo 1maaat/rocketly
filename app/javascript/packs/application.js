c2eaa774db45785d2f70ca744a088296718a8cb6
Pierre Bride <pierre_bride@icloud.com>
2021-08-25 18:32:28 +0200

    add multistep form at atwork

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -27,8 +29,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initArtworkform } from '../components/init_artworkform.js';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initArtworkform()
 });
