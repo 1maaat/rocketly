584cb42403cda8aea12c0e348ab7f632c53ee5ff
Walid Benzina <walid.benzina@yahoo.fr>
2021-08-27 02:08:14 +0200

    Items Aligned

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :artworks, dependent: :destroy
   has_many :user_activities, dependent: :destroy
   has_many :activities, through: :user_activities
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
