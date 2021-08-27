0edcf95ffabc28b5c7ebff3abc6c8bf0ab2e0bf3
Walid Benzina <walid.benzina@yahoo.fr>
2021-08-27 06:37:20 +0200

    Fix Migration issue with selected field in artworks

--- db/migrate/20210824123521_remove_activity_field_column_from_user.rb
+++ db/migrate/20210824123521_remove_activity_field_column_from_user.rb
@@ -1,7 +1,5 @@
 class RemoveActivityFieldColumnFromUser < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :activity_field
-    remove_column :artworks, :selected
-    add_column :artworks, :selected, default: false
   end
 end
