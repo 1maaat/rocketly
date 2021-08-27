910d37448c9b37616ced6acd8a8e66d9552843ae
Pierre Bride <pierre_bride@icloud.com>
2021-08-27 11:55:15 +0200

    drop create db

--- db/migrate/20210824123521_remove_activity_field_column_from_user.rb
+++ db/migrate/20210824123521_remove_activity_field_column_from_user.rb
@@ -2,6 +2,6 @@ class RemoveActivityFieldColumnFromUser < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :activity_field
     remove_column :artworks, :selected
-    add_column :artworks, :selected, default: false
+    add_column :artworks, :selected, :boolean, default: false
   end
 end
