d1c341a011d97597c429e809e1711254bfde20ab
Pierre Bride <pierre_bride@icloud.com>
2021-08-31 18:27:33 +0200

    fix pb js

--- app/javascript/components/profile_page.js
+++ app/javascript/components/profile_page.js
@@ -7,19 +7,21 @@ const editAvatar = () => {
   const imageUser = document.getElementById('output');
   const fakeInput = document.getElementById('fake-input');
   const input = document.querySelector('#input-user-pic > div > input');
-  fakeInput.addEventListener('click', () => {
-    input.click()
+  if (fakeInput)  {
+    fakeInput.addEventListener('click', () => {
+      input.click()
 
-  })
-  input.addEventListener("change", () => {
-    imageUser.src = URL.createObjectURL(input.files[0]);
-  })
-  const fakeUpdate = document.getElementById("fake-update");
-  const picForm = document.getElementById("user-pic-form");
+    })
+    input.addEventListener("change", () => {
+      imageUser.src = URL.createObjectURL(input.files[0]);
+    })
+    const fakeUpdate = document.getElementById("fake-update");
+    const picForm = document.getElementById("user-pic-form");
 
-  fakeUpdate.addEventListener("click", () => {
-    picForm.submit()
-  })
+    fakeUpdate.addEventListener("click", () => {
+      picForm.submit()
+    })
+  }
 }
 
 export {editAvatar}
