24476c3cb4e8f06719b5fa9d5b8bb5ab3168da92
Pierre Bride <pierre_bride@icloud.com>
2021-09-01 10:07:16 +0200

    added text-decoration none at application.scss

--- app/javascript/components/init_artworkform.js
+++ app/javascript/components/init_artworkform.js
@@ -26,6 +26,22 @@ function initArtworkform() {
         nextBtn.disabled = false
       }
     });
+
+    submitBtn.addEventListener("click"), (e) => {
+      e.preventDefault();
+      Swal.fire({
+        title: 'Custom width, padding, background.',
+        width: 600,
+        padding: '3em',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+                          rgba(0,0,123,0.4)
+                          url("/images/nyan-cat.gif")
+                          left top
+                          no-repeat
+                        `
+      });
+    }
   }
 
   function changeStep(button) {
