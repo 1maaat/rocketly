507ab00af9552eab65149a36e4ad901db14515d3
Pierre Bride <pierre_bride@icloud.com>
2021-09-02 13:05:22 +0200

    fix confetti zIndex

--- app/javascript/controllers/artwork_modal_controller.js
+++ app/javascript/controllers/artwork_modal_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "stimulus";
 import { csrfToken } from "@rails/ujs";
 import Swal from "sweetalert2";
+import confetti from 'canvas-confetti';
 
 export default class extends Controller {
   static targets = ["form", "finalSlide", "slide"]
@@ -9,8 +10,8 @@ export default class extends Controller {
   }
 
   send(event) {
+    showConfirmationConfettiDialog()
     event.preventDefault();
-    showConfirmationDialog()
     fetch(this.formTarget.action, {
       method: 'POST',
       headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
@@ -29,7 +30,8 @@ export default class extends Controller {
   };
 }
 
-function showConfirmationDialog() {
+function showConfirmationConfettiDialog() {
+  confettiAnimation()
   Swal.fire({
     toast: true,
     position: 'top-end',
@@ -38,12 +40,80 @@ function showConfirmationDialog() {
     showConfirmButton: false,
     padding: '3em',
     backdrop: `
-      rgba(0,0,123,0.1)
-      url("https://img1.picmix.com/output/stamp/normal/0/0/5/3/1503500_929b1.gif")
-      center
-      no-repeat
-      `
-    });
-  };
+    rgba(0,0,123,0.1)
+    left top
+    no-repeat
+    `
+  });
+};
+function confettiAnimation() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-  // url("https://sweetalert2.github.io/images/nyan-cat.gif")
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 250 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, zIndex: 0, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, zIndex: 0, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
+
+
+  var end = Date.now() + (100 * 1000);
+
+  // go Buckeyes!
+  var colors = ['#562877', '#533CF0'];
+
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors,
+      zIndex: 100000000000
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors,
+      zIndex: 100000000000
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  confetti({
+    angle: randomInRange(105, 125),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(250, 250),
+    origin: { y: 0.6 },
+    zIndex: 100000000000
+  });
+
+  confetti({
+    angle: randomInRange(55, 75),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(250, 250),
+    origin: { x: 0.5 },
+    zIndex: 100000000000
+  });
+};
